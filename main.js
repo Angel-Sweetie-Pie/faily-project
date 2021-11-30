facf46963df4f80d6bbebdf9cd5d290bf2eb6f12
Angel-Sweetie-Pie <79405067+Angel-Sweetie-Pie@users.noreply.github.com>
2021-11-30 20:27:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+body{
+     text-align: center;
+      background-inage: linear-gradient( pink, lavender, yellow);
+}
+       #family_img{
+            width:290px;
+       }
+              button{
+                   font-size:25px;
+                    background-color: bisque;
+                     border-radius:20%;
+                      color:black; 
+              }
+                      h1{
+                        font-size: 48px;
+                         color: blue;
+                          text-decoration: underline;
+                      }
+                          h3{
+                            font-size: 30px;
+                             color:purple;
+                              font-weight: bold;
+                               text-decoration: underline;   
+                          } 
+                      
